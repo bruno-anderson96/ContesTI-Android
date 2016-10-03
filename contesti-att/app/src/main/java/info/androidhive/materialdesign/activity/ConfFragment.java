488dc6db4f3a566d2df4845a77293aa2fc61700e
Anderson-PC <alencar_anderson@live.com>
2016-10-03 01:16:57 -0300

    Projeto Android

---
+++ contesti-att/app/src/main/java/info/androidhive/materialdesign/activity/ConfFragment.java
@@ -0,0 +1,42 @@
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.androidhive.contesti_ap.R;
+
+
+public class ConfFragment extends Fragment {
+
+    public ConfFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_conf, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
